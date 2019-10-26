5b70f76a88d5e2c1cbdd530389666c943ef6d16d
valentina <valentina_ferraioli@bitbucket.org>
2019-10-26 23:27:49 +0200

    burglary e raining in problog

--- anglican/burglar.clj
+++ anglican/burglar.clj
@@ -1,3 +1,6 @@
+;; gorilla-repl.fileformat = 1
+
+;; @@
 (ns bayes-net
   (:require [gorilla-plot.core :as plot]
             [anglican.stat :as s])
@@ -6,7 +9,7 @@
          [inference :only [collect-by]]]))
 
 (defquery burglar-bayes-net [alarm radio]
-  (let [earthquake (sample (flip 0.8))
+  (let [earthquake (sample (flip 0.4))
 
         burglar (cond (and (= alarm true )
                            (= radio false)) 
@@ -15,7 +18,7 @@
                               (= alarm true))
                          (sample (flip 0.2)))
         radio-dist (cond (= earthquake true) 
-                             (flip 0.8)
+                             (flip 0.7)
                              (= earthquake false) 
                              (flip 0.2))
         alarm-dist (cond 
@@ -27,14 +30,24 @@
                          (flip 0.01)
                      	 (or  (= burglar true) 
                               (= earthquake true))
-                         (flip 0.99))]
+                         (flip 0.70))]
     (observe radio-dist radio)
     (observe alarm-dist alarm)
 
     burglar))
+;using Lightweight Metropolis-Hastings inference function
 
-(->> (doquery :smc burglar-bayes-net [true false] :number-of-particles 100)
+(->> (doquery :lmh burglar-bayes-net [true true] :number-of-particles 100)
      (take 10000)
      (collect-by :result)
      (s/empirical-distribution)
      (#(plot/bar-chart (keys %) (vals %))))
+
+;; @@
+;; =>
+;;; {"type":"list-like","open":"","close":"","separator":"</pre><pre>","items":[{"type":"list-like","open":"","close":"","separator":"</pre><pre>","items":[{"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"},{"type":"html","content":"<span class='clj-var'>#&#x27;bayes-net/burglar-bayes-net</span>","value":"#'bayes-net/burglar-bayes-net"}],"value":"[nil,#'bayes-net/burglar-bayes-net]"},{"type":"vega","content":{"width":400,"height":247.2187957763672,"padding":{"top":10,"left":55,"bottom":40,"right":10},"data":[{"name":"0ec7f51a-3447-42b5-83c6-85fb44cd8266","values":[{"x":false,"y":0.6892000000000004},{"x":true,"y":0.31079999999999963}]}],"marks":[{"type":"rect","from":{"data":"0ec7f51a-3447-42b5-83c6-85fb44cd8266"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"width":{"scale":"x","band":true,"offset":-1},"y":{"scale":"y","field":"data.y"},"y2":{"scale":"y","value":0}},"update":{"fill":{"value":"steelblue"},"opacity":{"value":1}},"hover":{"fill":{"value":"#FF29D2"}}}}],"scales":[{"name":"x","type":"ordinal","range":"width","domain":{"data":"0ec7f51a-3447-42b5-83c6-85fb44cd8266","field":"data.x"}},{"name":"y","range":"height","nice":true,"domain":{"data":"0ec7f51a-3447-42b5-83c6-85fb44cd8266","field":"data.y"}}],"axes":[{"type":"x","scale":"x"},{"type":"y","scale":"y"}]},"value":"#gorilla_repl.vega.VegaView{:content {:width 400, :height 247.2188, :padding {:top 10, :left 55, :bottom 40, :right 10}, :data [{:name \"0ec7f51a-3447-42b5-83c6-85fb44cd8266\", :values ({:x false, :y 0.6892000000000004} {:x true, :y 0.31079999999999963})}], :marks [{:type \"rect\", :from {:data \"0ec7f51a-3447-42b5-83c6-85fb44cd8266\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :width {:scale \"x\", :band true, :offset -1}, :y {:scale \"y\", :field \"data.y\"}, :y2 {:scale \"y\", :value 0}}, :update {:fill {:value \"steelblue\"}, :opacity {:value 1}}, :hover {:fill {:value \"#FF29D2\"}}}}], :scales [{:name \"x\", :type \"ordinal\", :range \"width\", :domain {:data \"0ec7f51a-3447-42b5-83c6-85fb44cd8266\", :field \"data.x\"}} {:name \"y\", :range \"height\", :nice true, :domain {:data \"0ec7f51a-3447-42b5-83c6-85fb44cd8266\", :field \"data.y\"}}], :axes [{:type \"x\", :scale \"x\"} {:type \"y\", :scale \"y\"}]}}"}],"value":"[[nil,#'bayes-net/burglar-bayes-net],#gorilla_repl.vega.VegaView{:content {:width 400, :height 247.2188, :padding {:top 10, :left 55, :bottom 40, :right 10}, :data [{:name \"0ec7f51a-3447-42b5-83c6-85fb44cd8266\", :values ({:x false, :y 0.6892000000000004} {:x true, :y 0.31079999999999963})}], :marks [{:type \"rect\", :from {:data \"0ec7f51a-3447-42b5-83c6-85fb44cd8266\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :width {:scale \"x\", :band true, :offset -1}, :y {:scale \"y\", :field \"data.y\"}, :y2 {:scale \"y\", :value 0}}, :update {:fill {:value \"steelblue\"}, :opacity {:value 1}}, :hover {:fill {:value \"#FF29D2\"}}}}], :scales [{:name \"x\", :type \"ordinal\", :range \"width\", :domain {:data \"0ec7f51a-3447-42b5-83c6-85fb44cd8266\", :field \"data.x\"}} {:name \"y\", :range \"height\", :nice true, :domain {:data \"0ec7f51a-3447-42b5-83c6-85fb44cd8266\", :field \"data.y\"}}], :axes [{:type \"x\", :scale \"x\"} {:type \"y\", :scale \"y\"}]}}]"}
+;; <=
+
+;; @@
+
+;; @@
--- anglican/raining.clj
+++ anglican/raining.clj
@@ -1,40 +1,38 @@
 ;; gorilla-repl.fileformat = 1
 
 ;; @@
+;;;One morning Tracey leaves her house and realises that her grass is wet. Is it due to overnight rain or did she forget to turn off the sprinkler last night? Next she notices that the grass of her neighbour, Jack, is also wet. What's the probability that it has rained
+
 (ns bayes-net-raining
   (:require [gorilla-plot.core :as plot]
             [anglican.stat :as s])
   (:use clojure.repl
-        [anglican core runtime emit 
+        [anglican core runtime emit
          [inference :only [collect-by]]]))
 
 
 (defquery raining-b-net [t-wet-grass j-wet-grass]
-  (let [raining(cond(and (= t-wet-grass true)
-                         (= j-wet-grass true))
-                    	 (sample(flip 0.8))
-                     (or(= t-wet-grass false)
-                        (= j-wet-grass false))
-                      (sample(flip 0.01)))
+  (let [raining  (sample(flip 0.4))
 
         sprinkler (sample (flip 0.3))
         jack (cond (= raining true)
              		(flip 0.9)
                    (= raining false)
                    	(flip 0.2))
-        tracey(cond(and (= raining true) 
+                    
+        tracey(cond(and (= raining true)
                        	(= sprinkler true))
                         (flip 0.99)
-                   (and (= raining false) 
+                   (and (= raining false)
                         (= sprinkler false))
                         (flip 0.02)
-                   (or  (= raining true) 
+                   (or  (= raining true)
                         (= sprinkler true))
                         (flip 0.99))]
     (observe tracey t-wet-grass)
     (observe jack j-wet-grass)
 
-    sprinkler))
+    raining))
 
 
 (->> (doquery :smc raining-b-net [true false] :number-of-particles 100)
@@ -45,7 +43,7 @@
 
 ;; @@
 ;; =>
-;;; {"type":"list-like","open":"","close":"","separator":"</pre><pre>","items":[{"type":"list-like","open":"","close":"","separator":"</pre><pre>","items":[{"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"},{"type":"html","content":"<span class='clj-var'>#&#x27;bayes-net-raining/raining-b-net</span>","value":"#'bayes-net-raining/raining-b-net"}],"value":"[nil,#'bayes-net-raining/raining-b-net]"},{"type":"vega","content":{"width":400,"height":247.2187957763672,"padding":{"top":10,"left":55,"bottom":40,"right":10},"data":[{"name":"f2beeaec-db1c-4c94-9685-a07ff26183d9","values":[{"x":true,"y":0.9517590249580054},{"x":false,"y":0.048240975041994705}]}],"marks":[{"type":"rect","from":{"data":"f2beeaec-db1c-4c94-9685-a07ff26183d9"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"width":{"scale":"x","band":true,"offset":-1},"y":{"scale":"y","field":"data.y"},"y2":{"scale":"y","value":0}},"update":{"fill":{"value":"steelblue"},"opacity":{"value":1}},"hover":{"fill":{"value":"#FF29D2"}}}}],"scales":[{"name":"x","type":"ordinal","range":"width","domain":{"data":"f2beeaec-db1c-4c94-9685-a07ff26183d9","field":"data.x"}},{"name":"y","range":"height","nice":true,"domain":{"data":"f2beeaec-db1c-4c94-9685-a07ff26183d9","field":"data.y"}}],"axes":[{"type":"x","scale":"x"},{"type":"y","scale":"y"}]},"value":"#gorilla_repl.vega.VegaView{:content {:width 400, :height 247.2188, :padding {:top 10, :left 55, :bottom 40, :right 10}, :data [{:name \"f2beeaec-db1c-4c94-9685-a07ff26183d9\", :values ({:x true, :y 0.9517590249580054} {:x false, :y 0.048240975041994705})}], :marks [{:type \"rect\", :from {:data \"f2beeaec-db1c-4c94-9685-a07ff26183d9\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :width {:scale \"x\", :band true, :offset -1}, :y {:scale \"y\", :field \"data.y\"}, :y2 {:scale \"y\", :value 0}}, :update {:fill {:value \"steelblue\"}, :opacity {:value 1}}, :hover {:fill {:value \"#FF29D2\"}}}}], :scales [{:name \"x\", :type \"ordinal\", :range \"width\", :domain {:data \"f2beeaec-db1c-4c94-9685-a07ff26183d9\", :field \"data.x\"}} {:name \"y\", :range \"height\", :nice true, :domain {:data \"f2beeaec-db1c-4c94-9685-a07ff26183d9\", :field \"data.y\"}}], :axes [{:type \"x\", :scale \"x\"} {:type \"y\", :scale \"y\"}]}}"}],"value":"[[nil,#'bayes-net-raining/raining-b-net],#gorilla_repl.vega.VegaView{:content {:width 400, :height 247.2188, :padding {:top 10, :left 55, :bottom 40, :right 10}, :data [{:name \"f2beeaec-db1c-4c94-9685-a07ff26183d9\", :values ({:x true, :y 0.9517590249580054} {:x false, :y 0.048240975041994705})}], :marks [{:type \"rect\", :from {:data \"f2beeaec-db1c-4c94-9685-a07ff26183d9\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :width {:scale \"x\", :band true, :offset -1}, :y {:scale \"y\", :field \"data.y\"}, :y2 {:scale \"y\", :value 0}}, :update {:fill {:value \"steelblue\"}, :opacity {:value 1}}, :hover {:fill {:value \"#FF29D2\"}}}}], :scales [{:name \"x\", :type \"ordinal\", :range \"width\", :domain {:data \"f2beeaec-db1c-4c94-9685-a07ff26183d9\", :field \"data.x\"}} {:name \"y\", :range \"height\", :nice true, :domain {:data \"f2beeaec-db1c-4c94-9685-a07ff26183d9\", :field \"data.y\"}}], :axes [{:type \"x\", :scale \"x\"} {:type \"y\", :scale \"y\"}]}}]"}
+;;; {"type":"list-like","open":"","close":"","separator":"</pre><pre>","items":[{"type":"list-like","open":"","close":"","separator":"</pre><pre>","items":[{"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"},{"type":"html","content":"<span class='clj-var'>#&#x27;bayes-net-raining/raining-b-net</span>","value":"#'bayes-net-raining/raining-b-net"}],"value":"[nil,#'bayes-net-raining/raining-b-net]"},{"type":"vega","content":{"width":400,"height":247.2187957763672,"padding":{"top":10,"left":55,"bottom":40,"right":10},"data":[{"name":"858dcb09-6272-4fab-a9f5-02c62c40b950","values":[{"x":false,"y":0.7873175655535822},{"x":true,"y":0.2126824344464177}]}],"marks":[{"type":"rect","from":{"data":"858dcb09-6272-4fab-a9f5-02c62c40b950"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"width":{"scale":"x","band":true,"offset":-1},"y":{"scale":"y","field":"data.y"},"y2":{"scale":"y","value":0}},"update":{"fill":{"value":"steelblue"},"opacity":{"value":1}},"hover":{"fill":{"value":"#FF29D2"}}}}],"scales":[{"name":"x","type":"ordinal","range":"width","domain":{"data":"858dcb09-6272-4fab-a9f5-02c62c40b950","field":"data.x"}},{"name":"y","range":"height","nice":true,"domain":{"data":"858dcb09-6272-4fab-a9f5-02c62c40b950","field":"data.y"}}],"axes":[{"type":"x","scale":"x"},{"type":"y","scale":"y"}]},"value":"#gorilla_repl.vega.VegaView{:content {:width 400, :height 247.2188, :padding {:top 10, :left 55, :bottom 40, :right 10}, :data [{:name \"858dcb09-6272-4fab-a9f5-02c62c40b950\", :values ({:x false, :y 0.7873175655535822} {:x true, :y 0.2126824344464177})}], :marks [{:type \"rect\", :from {:data \"858dcb09-6272-4fab-a9f5-02c62c40b950\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :width {:scale \"x\", :band true, :offset -1}, :y {:scale \"y\", :field \"data.y\"}, :y2 {:scale \"y\", :value 0}}, :update {:fill {:value \"steelblue\"}, :opacity {:value 1}}, :hover {:fill {:value \"#FF29D2\"}}}}], :scales [{:name \"x\", :type \"ordinal\", :range \"width\", :domain {:data \"858dcb09-6272-4fab-a9f5-02c62c40b950\", :field \"data.x\"}} {:name \"y\", :range \"height\", :nice true, :domain {:data \"858dcb09-6272-4fab-a9f5-02c62c40b950\", :field \"data.y\"}}], :axes [{:type \"x\", :scale \"x\"} {:type \"y\", :scale \"y\"}]}}"}],"value":"[[nil,#'bayes-net-raining/raining-b-net],#gorilla_repl.vega.VegaView{:content {:width 400, :height 247.2188, :padding {:top 10, :left 55, :bottom 40, :right 10}, :data [{:name \"858dcb09-6272-4fab-a9f5-02c62c40b950\", :values ({:x false, :y 0.7873175655535822} {:x true, :y 0.2126824344464177})}], :marks [{:type \"rect\", :from {:data \"858dcb09-6272-4fab-a9f5-02c62c40b950\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :width {:scale \"x\", :band true, :offset -1}, :y {:scale \"y\", :field \"data.y\"}, :y2 {:scale \"y\", :value 0}}, :update {:fill {:value \"steelblue\"}, :opacity {:value 1}}, :hover {:fill {:value \"#FF29D2\"}}}}], :scales [{:name \"x\", :type \"ordinal\", :range \"width\", :domain {:data \"858dcb09-6272-4fab-a9f5-02c62c40b950\", :field \"data.x\"}} {:name \"y\", :range \"height\", :nice true, :domain {:data \"858dcb09-6272-4fab-a9f5-02c62c40b950\", :field \"data.y\"}}], :axes [{:type \"x\", :scale \"x\"} {:type \"y\", :scale \"y\"}]}}]"}
 ;; <=
 
 ;; @@
